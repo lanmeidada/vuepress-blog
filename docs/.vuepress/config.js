9a769563d93a4b754a71c728f2298e4396e60fef
lanmeidada <lanmeidada@qq.com>
2019-12-28 18:23:26 +0800

    更改配置，新增文章

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,22 +14,19 @@ module.exports = {
     lastUpdated: 'Last Updated' ,// 文档更新时间：每个文件git最后提交的时间
     logo: '/logo.gif',
     smoothScroll: true, // 启用页面滚动效果
+    activeHeaderLinks: false, // 用户通过滚动查看页面的不同部分时，嵌套的标题链接和 URL 中的 Hash 值会实时更新,默认值：true
       // 顶部导航栏配置
     nav: [
       // 单项 text：显示文字，link：指向链接
       // 这里的'/' 指的是 docs文件夹路径
       // [以 '/' 结尾的默认指向该路径下README.md文件]
       {
-        text: 'javaScript',
-        link: '/javaScript/js去除字符串空白字符' // '/'默认为docs文件夹下的README.md文件
-      },
-      {
         text: 'windows',
         link: '/windows/powershell配置'
       },
       {
-        text: 'version',
-        link: '/version'
+        text: 'javaScript',
+        link: '/javaScript/js去除字符串空白字符' // '/'默认为docs文件夹下的README.md文件
       },
       {
         text: '友情链接',
--- docs/windows/node多版本控制.md
+++ docs/windows/node多版本控制.md
@@ -1,1 +1,25 @@
 # node多版本控制
+## 通过scoop安装nvs
+```shell script
+scoop install nvs
+```
+## 帮助命令
+```shell script
+nvs --help 
+```
+## 安装最新的lts版本
+```shell script
+nvs add lts
+```
+## 安装固定版本，例如10
+```shell script
+nvs add 10
+```
+## 当前shell切换node的10版本
+```shell script
+nvs use 10
+```
+## 设置全局默认node版本为lts
+```shell script
+nvs link lts
+```
